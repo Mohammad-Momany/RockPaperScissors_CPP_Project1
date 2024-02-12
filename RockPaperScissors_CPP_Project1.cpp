ace19bba893b56aed8371a75d4a6af6dc34f9dd9
Mohammad-Momany <mohammad.w.momani@gmail.com>
2024-02-12 16:39:23 +0300

    1. Complete the User inputs. 2. Find the result and change the background color. 3. repate the game depending in the user input.

--- RockPaperScissors_CPP_Project1.cpp
+++ RockPaperScissors_CPP_Project1.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
-
+#include <windows.h>
 using namespace std;
 
+
+enum enumGameOptions { Invalid = 0, Rock = 1, Scissors = 2, Paper = 3};
+
+struct structGameResult { 
+	int DrawTimes;
+	int WinTimes;
+	int LoseTimes;
+};
 
 string ToLowerCase(string word) {
 
@@ -16,10 +24,27 @@ string ToLowerCase(string word) {
 
 	return word;
 }
+int RandomNumber(int From, int To) {
+	// Function to generate a random number
+	int randNum = rand() % (To - From + 1) + From;
+	return randNum;
+}
 
-enum enumGameOptions { Invalid = 0, Rock = 1, Scissors = 2, Paper = 3};
+int ReadFromToPositiveNumber(string Message, int From, int To) 
+{
+	int InputNumber = 0;
+	do
+	{
+		cout << Message;
+		cin >> InputNumber;
+	} while (InputNumber < From || InputNumber > To);
 
-enumGameOptions ConvertStringToEnumGameOptions(string input) {
+	return InputNumber;
+}
+
+
+enumGameOptions ConvertInputToEnumGameOptions(string input) {
+
 	input = ToLowerCase(input);
 
 
@@ -37,28 +62,69 @@ enumGameOptions ConvertStringToEnumGameOptions(string input) {
 	}
 
 }
-string ReadString(string Message) {
+
+enumGameOptions ReadGameOptions(string Message) {
 
 	string Option = "";
+	enumGameOptions GameOption;
 	do
 	{
-		
 		cout << Message;
 		cin >> Option;
+		GameOption = ConvertInputToEnumGameOptions(Option);
 
-	} while (Option != "");
+	} while (GameOption == enumGameOptions::Invalid);
 
-	return Option;
+	return GameOption;
 }
 
-int RandomNumber(int From, int To) {
-	// Function to generate a random number
-	int randNum = 1 + rand() % (To - From + 1) + From;
-	return randNum;
+
+void CountGameResult(enumGameOptions UserChoose, int ComputerChoose, structGameResult &GameResult) {
+
+	if (UserChoose == ComputerChoose)
+	{
+		system("Color 60");
+		cout << "Draw";
+		GameResult.DrawTimes++;
+	}
+	else if (UserChoose > ComputerChoose)
+	{
+		system("Color 20");
+		cout << "You Win!";
+		GameResult.WinTimes++;
+	}
+	else
+	{
+		system("Color 40");
+		cout << "Game Over!";
+		GameResult.LoseTimes++;
+	}
 }
 
+structGameResult GameCompetition(int GameTimes)
+{
+	structGameResult GameResult;
+
+	for (int i = 0; i < GameTimes; i++)
+	{
+		enumGameOptions UserChoose = ReadGameOptions("Plase Enter your choose: [1] Rock, [2] Paper, [3] Scissors\n");
+
+		int RandomNum = RandomNumber(1, 3);
+
+		CountGameResult(UserChoose, RandomNum, GameResult);
+	}
+	return GameResult;
+}
 int main()
 {
-	int UserChoose;
-	cout << ToLowerCase("MohammaD");
+	srand((unsigned)time(NULL));
+
+	int GameTimes = ReadFromToPositiveNumber("Please enter the game time from 1 to 10\n", 1, 10);
+
+	structGameResult GameResult = GameCompetition(GameTimes);
+
+	cout << "WinTimes: " << GameResult.WinTimes << "\n";
+	cout << "DrawTimes: " << GameResult.DrawTimes << "\n";
+	cout << "LoseTimes: " << GameResult.LoseTimes << "\n";
+
 }
